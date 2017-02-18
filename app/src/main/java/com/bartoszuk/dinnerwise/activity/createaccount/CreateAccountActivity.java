133e345ec9a5287c6865ace99ff08381b9bd5056
mbartoszuk <mariabartoszuk@gmail.com>
2017-02-18 21:09:36 +0000

    Linking Between Create Account and Login Activities

--- app/src/main/java/com/bartoszuk/dinnerwise/activity/createaccount/CreateAccountActivity.java
+++ app/src/main/java/com/bartoszuk/dinnerwise/activity/createaccount/CreateAccountActivity.java
@@ -4,16 +4,24 @@ package com.bartoszuk.dinnerwise.activity.createaccount;
  * Created by Maria Bartoszuk on 18/02/2017.
  */
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bartoszuk.dinnerwise.R;
+import com.bartoszuk.dinnerwise.activity.login.LoginActivity;
 
-public class CreateAccountActivity extends Activity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void goToLogin(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/bartoszuk/dinnerwise/activity/login/LoginActivity.java
+++ app/src/main/java/com/bartoszuk/dinnerwise/activity/login/LoginActivity.java
@@ -4,16 +4,28 @@ package com.bartoszuk.dinnerwise.activity.login;
  * Created by Maria Bartoszuk on 18/02/2017.
  */
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bartoszuk.dinnerwise.R;
+import com.bartoszuk.dinnerwise.activity.createaccount.CreateAccountActivity;
+import com.google.android.gms.common.SignInButton;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignInButton signInWithGoogleButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInWithGoogleButton.setSize(SignInButton.SIZE_WIDE);
+    }
+
+    public void goToCreateAccount(View view) {
+        startActivity(new Intent(this, CreateAccountActivity.class));
+        finish();
     }
 }
