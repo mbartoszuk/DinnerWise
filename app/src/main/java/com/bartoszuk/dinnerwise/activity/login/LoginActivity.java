74c76caac5ff189cd6bc6e60de17f65efec2f4aa
mbartoszuk <mariabartoszuk@gmail.com>
2017-02-18 19:14:49 +0000

    Create Account Field Layout

---
+++ app/src/main/java/com/bartoszuk/dinnerwise/activity/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.bartoszuk.dinnerwise.activity.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bartoszuk.dinnerwise.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
